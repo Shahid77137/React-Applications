bf4041ff37228ae97a2aa3bb4de23210b660b607
Shahid Afridi <siddiquibabuddin202@gmail.com>
2023-09-14 12:37:33 +0530

    React Props application

---
+++ myapp-props/module2.mjs
@@ -0,0 +1,11 @@
+const a = "Shahid";
+const b = "Afridi";
+const c = "Priyanka";
+const d = "Subiksha";
+const e = "Rahul Kumar";
+
+export default d;
+export {a};
+export {b};
+export {c};
+export {e};
---
+++ myapp-props/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+  
+  <>
+
+<Navbar title="TextUtils" homeText="Home" aboutText="About Us" />
+
+{/* <Navbar  /> */}
+  </>
+  );
+}
+
+export default App;
